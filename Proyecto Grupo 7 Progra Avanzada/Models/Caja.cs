83d07ece9bff364d6cb45ede541c82f3e5c2f0ae
V-i-ctor <114272340+V-i-ctor@users.noreply.github.com>
2025-11-03 15:49:41 -0600

    Modelado de Caja
    Creacion de Modelo Caja

---
+++ Proyecto Grupo 7 Progra Avanzada/Models/Caja.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Grupo_7_Progra_Avanzada.Models
+{
+    public class Caja
+    {
+        public int IdCaja { get; set; }
+
+        [Required]
+        public int IdComercio { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [StringLength(255)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [Phone]
+        public string TelefonoSINPE { get; set; }
+
+        public DateTime FechaDeRegistro { get; set; } = DateTime.Now;
+
+        public DateTime? FechaDeModificacion { get; set; }
+
+        public bool Estado { get; set; } = true;
+
+        // Propiedad de Navegacion
+        public Comercio Comercio { get; set; }
+
+    }
+}
