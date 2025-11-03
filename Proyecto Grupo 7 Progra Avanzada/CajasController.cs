b3e58bcd2791eea2b41be32a44c54ae64890abcc
V-i-ctor <114272340+V-i-ctor@users.noreply.github.com>
2025-11-03 16:01:33 -0600

    Caja CRUD
    Se agrega vistas y controlador para las operaciones CRUD del modelo Caja.

---
+++ Proyecto Grupo 7 Progra Avanzada/CajasController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Proyecto_Grupo_7_Progra_Avanzada.Data;
+using Proyecto_Grupo_7_Progra_Avanzada.Models;
+
+namespace Proyecto_Grupo_7_Progra_Avanzada
+{
+    public class CajasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CajasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cajas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cajas.ToListAsync());
+        }
+
+        // GET: Cajas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var caja = await _context.Cajas
+                .FirstOrDefaultAsync(m => m.IdCaja == id);
+            if (caja == null)
+            {
+                return NotFound();
+            }
+
+            return View(caja);
+        }
+
+        // GET: Cajas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cajas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCaja,IdComercio,Nombre,Descripcion,TelefonoSINPE,FechaDeRegistro,FechaDeModificacion,Estado")] Caja caja)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(caja);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(caja);
+        }
+
+        // GET: Cajas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var caja = await _context.Cajas.FindAsync(id);
+            if (caja == null)
+            {
+                return NotFound();
+            }
+            return View(caja);
+        }
+
+        // POST: Cajas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCaja,IdComercio,Nombre,Descripcion,TelefonoSINPE,FechaDeRegistro,FechaDeModificacion,Estado")] Caja caja)
+        {
+            if (id != caja.IdCaja)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(caja);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CajaExists(caja.IdCaja))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(caja);
+        }
+
+        // GET: Cajas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var caja = await _context.Cajas
+                .FirstOrDefaultAsync(m => m.IdCaja == id);
+            if (caja == null)
+            {
+                return NotFound();
+            }
+
+            return View(caja);
+        }
+
+        // POST: Cajas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var caja = await _context.Cajas.FindAsync(id);
+            if (caja != null)
+            {
+                _context.Cajas.Remove(caja);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CajaExists(int id)
+        {
+            return _context.Cajas.Any(e => e.IdCaja == id);
+        }
+    }
+}
