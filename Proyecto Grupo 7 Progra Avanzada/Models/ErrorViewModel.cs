392943f5292be85d8748836eb0f66ff2c5853086
lucimo12 <luciamora028@gmail.com>
2025-12-10 21:45:20 -0600

    Delete Proyecto Grupo 7 Progra Avanzada directory

--- Proyecto Grupo 7 Progra Avanzada/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proyecto_Grupo_7_Progra_Avanzada.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
